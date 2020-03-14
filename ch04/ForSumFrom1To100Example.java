803dcc917e07c6d475735a6b45c654cca0ef130d
wisehero <54886222+wisehero@users.noreply.github.com>
2020-03-14 22:19:33 +0900

    Add files via upload

---
+++ ch04/ForSumFrom1To100Example.java
@@ -0,0 +1,15 @@
+package ch04;
+
+public class ForSumFrom1To100Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		
+		for(int i=1; i<=100; i++) {
+			sum += i;
+		}
+		System.out.println("1~100 합: " + sum);
+	}
+
+}
---
+++ ch04/practice04.java
@@ -0,0 +1,21 @@
+package ch04;
+
+public class practice04 {
+	
+	public static void main(String[] args) {
+		
+	
+		
+		while(true) {
+			int num2 = (int) (Math.random() * 6) +1;
+			int num1 = (int) (Math.random() * 6) +1;
+			System.out.println("(" + num1 + ", " + num2 + ")");
+			
+			if((num1+num2) !=5) {
+				break;
+			}
+			
+		}
+	}
+
+}
