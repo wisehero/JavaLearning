e5e2f8b9cdd4b340c4fc693a48ff52c281e38cf9
wisehero <54886222+wisehero@users.noreply.github.com>
2020-03-16 15:54:24 +0900

    Create AdvancedForExample.java

---
+++ ch05/AdvancedForExample.java
@@ -0,0 +1,21 @@
+package ch05;
+
+public class AdvancedForExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[] scores = { 95, 71, 84, 93, 87};
+		
+		int sum = 0;
+		for (int score : scores) {
+			sum += score;
+		}
+		System.out.println("점수 총합 = " + sum);
+		
+	    double avg = (double) sum / scores.length;
+	    System.out.println("점수 평균 = " + avg);
+
+	}
+
+}
