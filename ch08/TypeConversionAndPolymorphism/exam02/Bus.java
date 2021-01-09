5a168783e4f31a58ec22ec19df30c652d7effcad
wisehero <godjw96@gmail.com>
2021-01-10 03:33:29 +0900

    Create Bus.java

---
+++ ch08/TypeConversionAndPolymorphism/exam02/Bus.java
@@ -0,0 +1,8 @@
+package ch08.TypeConversionAndPolymorphism.exam02;
+
+public class Bus implements Vehicle {
+    @Override
+    public void run() {
+        System.out.println("버스가 달립니다.");
+    }
+}
