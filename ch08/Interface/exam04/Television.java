eb62a251145832fcc1a1d39fbeb0a1b25a79c9a4
wisehero <godjw96@gmail.com>
2021-01-09 12:48:37 +0900

    Create Television.java

---
+++ ch08/Interface/exam04/Television.java
@@ -0,0 +1,30 @@
+package ch08.Interface.exam04;
+
+import ch08.Interface.exam03.RemoteControl;
+
+public class Television implements RemoteControl {
+    // 필드
+    private int volume;
+
+    // turnOn() 추상 메소드의 실체 메소드
+    public void turnOn() {
+        System.out.println("TV를 켭니다.");
+    }
+    // turnOff() 추상 메소드의 실체 메소드
+    public void turnOff() {
+        System.out.println("TV를 끕니다.");
+    }
+    // setVolume() 추상 메소드의 실체 메소
+    public void setVolume(int volume) {
+
+        // 인터페이스의 상수를 이용해서 volume 필드의 값을 제한
+        if(volume > RemoteControl.MAX_VOLUME) {
+            this.volume = RemoteControl.MAX_VOLUME;
+        } else if (volume < RemoteControl.MIN_VOLUME){
+            this.volume = RemoteControl.MIN_VOLUME;
+        } else {
+            this.volume = volume;
+        }
+        System.out.println("현재 Tv 볼륨: " + this.volume);
+    }
+}
