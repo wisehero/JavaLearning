4b5e4c2255360be0edc2095623f60f7c5f6406d6
wisehero <godjw96@gmail.com>
2021-01-09 12:48:26 +0900

    Create RemoteControl.java

---
+++ ch08/Interface/exam03/RemoteControl.java
@@ -0,0 +1,13 @@
+package ch08.Interface.exam03;
+
+public interface RemoteControl {
+    // 상수
+    public int MAX_VOLUME = 10;
+    public int MIN_VOLUME = 0;
+
+    // 추상 메소드
+    // 선언부만 작성
+    public void turnOn();
+    public void turnOff();
+    public void setVolume(int volume);
+}
