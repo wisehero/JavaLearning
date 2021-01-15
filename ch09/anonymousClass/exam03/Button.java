29b440424ac0ba54b38b3da793bcc61d11b5f38d
wisehero <godjw96@gmail.com>
2021-01-15 14:11:54 +0900

    Create Button.java

---
+++ ch09/anonymousClass/exam03/Button.java
@@ -0,0 +1,16 @@
+package ch09.anonymousClass.exam03;
+
+public class Button {
+    OnClickListener listener;
+
+    void setOnClickListener(OnClickListener listener) {
+        this.listener = listener;
+    }
+
+    void touch() {
+        listener.onClick();
+    }
+    static interface OnClickListener {
+        void onClick();
+    }
+}
