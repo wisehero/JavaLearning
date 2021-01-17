c192809125ff862e364cc88b99ded410e53c51c7
wisehero <godjw96@gmail.com>
2021-01-17 13:55:18 +0900

    Create OutterExample.java

---
+++ ch09/nestedClassAndNestedInterface/exam05/OutterExample.java
@@ -0,0 +1,9 @@
+package ch09.nestedClassAndNestedInterface.exam05;
+
+public class OutterExample {
+    public static void main(String[] args) {
+        Outter outter = new Outter();
+        Outter.Nested nested = outter.new Nested();
+        nested.print();
+    }
+}
