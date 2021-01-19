847ec4875892086706ef9f344b3d5c4332f80995
wisehero <godjw96@gmail.com>
2021-01-19 22:09:47 +0900

    Create MessageListener.java

---
+++ ch09/nestedClassAndNestedInterface/exam06/MessageListener.java
@@ -0,0 +1,9 @@
+package ch09.nestedClassAndNestedInterface.exam06;
+
+public class MessageListener implements Button.OnClickListener {
+    @Override
+    public void onClick() {
+        System.out.println("메시지를 보냅니다.");
+    }
+
+}
