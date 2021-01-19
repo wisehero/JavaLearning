86b5475ca34672b4709429e46c7d0febae250684
wisehero <godjw96@gmail.com>
2021-01-19 22:09:43 +0900

    Create CallListener.java

---
+++ ch09/nestedClassAndNestedInterface/exam06/CallListener.java
@@ -0,0 +1,8 @@
+package ch09.nestedClassAndNestedInterface.exam06;
+
+public class CallListener implements Button.OnClickListener {
+    @Override
+    public void onClick() {
+        System.out.println("전화를 겁니다.");
+    }
+}
