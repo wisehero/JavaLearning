d70a3655369934047a49b4f5367bc879cffd1b54
wisehero <godjw96@gmail.com>
2021-01-27 14:15:50 +0900

    Create MemberExample.java

---
+++ ch11/Practice/MemberExample.java
@@ -0,0 +1,8 @@
+package ch11.Practice;
+
+public class MemberExample {
+    public static void main(String[] args) {
+        Member member = new Member("blue", "이파란");
+        System.out.println(member);
+    }
+}
