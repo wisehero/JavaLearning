a538f51d1dde617cca27b3983c6a703df969e392
wisehero <godjw96@gmail.com>
2021-02-01 12:44:26 +0900

    Create PrintThread2.java

---
+++ ch12/threadControl/exam03/PrintThread2.java
@@ -0,0 +1,15 @@
+package ch12.threadControl.exam03;
+
+public class PrintThread2 extends Thread{
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("실행 중");
+                Thread.sleep(1);
+            }
+        } catch (InterruptedException e) {}
+
+        System.out.println("자원 정리");
+        System.out.println("실행 중");
+    }
+}
