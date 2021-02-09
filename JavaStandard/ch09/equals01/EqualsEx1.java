8a14f734de11ea2e5b94fe61f6cd9ea1607f2a27
wisehero <godjw96@gmail.com>
2021-02-09 22:29:42 +0900

    Create EqualsEx1.java

---
+++ JavaStandard/ch09/equals01/EqualsEx1.java
@@ -0,0 +1,30 @@
+package JavaStandard.ch09.equals01;
+
+// Object 클래스의 equals 메서드에 대한 코드
+
+public class EqualsEx1 {
+    public static void main(String[] args) {
+        Value v1 = new Value(10);
+        Value v2 = new Value(10);
+
+        if(v1.equals(v2))
+            System.out.println("v1과 v2는 같습니다");
+        else
+            System.out.println("v1과 v2는 다릅니다.");
+
+        v2 = v1;
+
+        if(v1.equals(v2))
+            System.out.println("v1과 v2는 같습니다");
+        else
+            System.out.println("v1과 v2는 다릅니다.");
+    }
+}
+
+class Value {
+    int value;
+
+    Value(int value) {
+        this.value = value;
+    }
+}
