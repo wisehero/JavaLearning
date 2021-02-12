9d74757d0a61e575341e4295e5133c973c2a431b
wisehero <godjw96@gmail.com>
2021-02-13 05:33:03 +0900

    Create StringEx4.java

---
+++ JavaStandard/ch09/String/StringEx4.java
@@ -0,0 +1,9 @@
+package JavaStandard.ch09.String;
+
+public class StringEx4 {
+    public static void main(String[] args) {
+        String animals = "dog, cat, bear";
+        String[] arr = animals.split(",");
+
+    }
+}
