a964f59bab225c51b07af696c3fa46fd4d747047
김지웅 <godjw96@gmail.com>
2021-02-20 03:02:07 +0900

    Create Coin.java

---
+++ ch13/exam09/Coin.java
@@ -0,0 +1,13 @@
+package ch13.exam09;
+
+public class Coin {
+    private int value;
+
+    public Coin(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
