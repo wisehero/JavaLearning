2cb6e5188cdd5efe5b1897de9892b45834282fe9
김지웅 <wisehero@gimjiung-ui-MacBookPro.local>
2021-02-19 16:42:01 +0900

    Create HashMapExample.java

---
+++ ch13/exam07/HashMapExample.java
@@ -0,0 +1,15 @@
+package ch13.exam07;
+
+import java.util.*;
+
+public class HashMapExample {
+    public static void main(String[] args) {
+        Map<Student, Integer> map = new HashMap<>();
+
+        map.put(new Student(1, "홍길동"), 95);
+        map.put(new Student(1, "홍길동"), 95);
+
+        System.out.println("총 Entry 수: " + map.size());
+
+    }
+}
