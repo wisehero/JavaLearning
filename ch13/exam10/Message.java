88f4e9bf93f19c37d7ac9f80832e4982ff1514e3
김지웅 <godjw96@gmail.com>
2021-02-23 03:23:21 +0900

    Create Message.java

---
+++ ch13/exam10/Message.java
@@ -0,0 +1,11 @@
+package ch13.exam10;
+
+public class Message {
+    public String command;
+    public String to;
+
+    public Message(String command, String to) {
+        this.command = command;
+        this.to = to;
+    }
+}
