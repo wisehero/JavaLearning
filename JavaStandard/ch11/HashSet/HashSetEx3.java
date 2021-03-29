06988b8fa35636da260088d33d126e715cc400c4
김지웅 <godjw96@gmail.com>
2021-03-29 17:17:57 +0900

    Create HashSetEx3.java

---
+++ JavaStandard/ch11/HashSet/HashSetEx3.java
@@ -0,0 +1,28 @@
+package JavaStandard.ch11.HashSet;
+
+import java.util.*;
+
+public class HashSetEx3 {
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        set.add("abc");
+        set.add("abc");
+        set.add(new Person("David", 10));
+        set.add(new Person("David", 10));
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + ":" + age;
+    }
+}
