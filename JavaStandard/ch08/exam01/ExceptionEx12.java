b8a5d657639035187c03107c686b18daf422326e
김지웅 <godjw96@gmail.com>
2021-03-31 20:11:02 +0900

    Create ExceptionEx12.java

---
+++ JavaStandard/ch08/exam01/ExceptionEx12.java
@@ -0,0 +1,15 @@
+package JavaStandard.ch08.exam01;
+
+public class ExceptionEx12 {
+    public static void main(String[] args) throws Exception {
+        method1();
+    }
+
+    static void method1() throws Exception {
+        method2();
+    }
+
+    static void method2() throws Exception {
+        throw new Exception();
+    }
+}
