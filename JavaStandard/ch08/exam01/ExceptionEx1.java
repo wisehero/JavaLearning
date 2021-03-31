f934f2b6c3b2e1547ef91dd679f81b4b8a49b74a
김지웅 <godjw96@gmail.com>
2021-03-31 20:10:58 +0900

    Create ExceptionEx1.java

---
+++ JavaStandard/ch08/exam01/ExceptionEx1.java
@@ -0,0 +1,20 @@
+package JavaStandard.ch08.exam01;
+
+public class ExceptionEx1 {
+    public static void main(String[] args) {
+        try {
+            try {
+            } catch (Exception e) {
+            }
+        } catch (Exception e) {
+//            try {
+//            } catch (Exception e) {
+//            } // 에러. 변수 e가 중복 선언되었다.
+        } // try - catch의 끝
+
+        try {
+
+        } catch (Exception e) {
+        }
+    }
+}
