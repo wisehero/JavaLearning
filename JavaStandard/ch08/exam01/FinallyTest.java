c88d3484c7b5376bab49783fcb12e9d334358abf
김지웅 <godjw96@gmail.com>
2021-04-01 07:28:18 +0900

    Create FinallyTest.java

---
+++ JavaStandard/ch08/exam01/FinallyTest.java
@@ -0,0 +1,19 @@
+package JavaStandard.ch08.exam01;
+
+public class FinallyTest {
+    public static void main(String[] args) {
+        method1();
+        System.out.println("method1()의 수행을 마치고 main메서드로 돌아왔습니다.");
+    }
+
+    static void method1() {
+        try {
+            System.out.println("method1()이 호출되었습니다.");
+            return;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("method1()의 finally블럭이 실행되었습니다.");
+        }
+    }
+}
