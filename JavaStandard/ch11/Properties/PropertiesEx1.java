1591f3675a25de8aeffc1d28f4380f9c7762758b
김지웅 <godjw96@gmail.com>
2021-04-02 13:42:43 +0900

    Create PropertiesEx1.java

---
+++ JavaStandard/ch11/Properties/PropertiesEx1.java
@@ -0,0 +1,33 @@
+package JavaStandard.ch11.Properties;
+
+import java.util.*;
+
+public class PropertiesEx1 {
+    public static void main(String[] args) {
+        Properties prop = new Properties();
+
+        // prop에 키와 값(key, value)을 저장한다.
+        prop.setProperty("timeout", "30");
+        prop.setProperty("language", "kr");
+        prop.setProperty("size", "10");
+        prop.setProperty("capacity", "10");
+
+        // prop에 저장된 요소들을 Enumeration을 이용해서 출력
+
+        Enumeration e = prop.propertyNames();
+
+        while (e.hasMoreElements()) {
+            String element = (String) e.nextElement();
+            System.out.println(element + "=" + prop.getProperty(element));
+        }
+        
+        System.out.println();
+        prop.setProperty("size", "20"); // size의 값을 20으로 변경한다.
+        System.out.println("size = " + prop.getProperty("size"));
+        System.out.println("capacity = " + prop.getProperty("capacity", "20"));
+        System.out.println("loadfactor = " + prop.getProperty("loadfactor", "0.75"));
+
+        System.out.println(prop);
+        prop.list(System.out);
+    }
+}
