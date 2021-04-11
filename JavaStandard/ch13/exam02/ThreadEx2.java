f475a3ecefa8ee28ae7fa53478c6143925677011
김지웅 <godjw96@gmail.com>
2021-04-11 18:05:21 +0900

    Create ThreadEx2.java

---
+++ JavaStandard/ch13/exam02/ThreadEx2.java
@@ -0,0 +1,22 @@
+package JavaStandard.ch13.exam02;
+
+public class ThreadEx2 {
+    public static void main(String[] args) {
+        ThreadEx2_1 t1 = new ThreadEx2_1();
+        t1.start();
+    }
+}
+
+class ThreadEx2_1 extends Thread {
+    public void run() {
+        throwException();
+    }
+
+    public void throwException() {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
