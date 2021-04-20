88f79903a776a0514a51cab171985a32c13f3774
김지웅 <godjw96@gmail.com>
2021-04-20 16:15:30 +0900

    Create ThreadEx14.java

---
+++ JavaStandard/ch13/exam14/ThreadEx14.java
@@ -0,0 +1,30 @@
+package JavaStandard.ch13.exam14;
+
+import javax.swing.*;
+
+public class ThreadEx14 {
+    public static void main(String[] args) throws Exception {
+        ThreadEx14_1 th1 = new ThreadEx14_1();
+        th1.start();
+
+        String input = JOptionPane.showInputDialog("아무 값이나 입력하세요.");
+        System.out.println("입력하신 값은 " + input + "입니다.");
+        th1.interrupt();
+        System.out.println("isInterrupted():" + th1.isInterrupted());
+    }
+}
+
+class ThreadEx14_1 extends Thread {
+    public void run() {
+        int i = 10;
+
+        while (i != 0 && !isInterrupted()) {
+            System.out.println(i--);
+            try {
+                Thread.sleep(1000); // 1초 지연
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("카운트가 종료되었습니다.");
+    }
+}
