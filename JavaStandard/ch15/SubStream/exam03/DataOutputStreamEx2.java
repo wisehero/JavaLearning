7a9afeec8b9304c303838415d483cfa9799542e0
김지웅 <godjw96@gmail.com>
2021-05-06 01:05:32 +0900

    Create DataOutputStreamEx2.java

---
+++ JavaStandard/ch15/SubStream/exam03/DataOutputStreamEx2.java
@@ -0,0 +1,36 @@
+package JavaStandard.ch15.SubStream.exam03;
+
+import java.io.*;
+
+public class DataOutputStreamEx2 {
+    public static void main(String[] args) {
+        ByteArrayOutputStream bos = null;
+        DataOutputStream dos = null;
+
+        byte[] result = null;
+
+        try {
+            bos = new ByteArrayOutputStream();
+            dos = new DataOutputStream(bos);
+            dos.writeInt(10);
+            dos.writeFloat(20.0f);
+            dos.writeBoolean(true);
+
+            result = bos.toByteArray();
+
+            String[] hex = new String[result.length];
+
+            for (int i = 0; i < result.length; i++) {
+                if (result[i] < 0) {
+                    hex[i] = String.format("%02x", result[i] + 256);
+                } else {
+                    hex[i] = String.format("%02x", result[i]);
+                }
+            }
+
+            dos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
